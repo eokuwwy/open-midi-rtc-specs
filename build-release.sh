da0911f89e5d61f7d45ac47a5120a37311854f82
Steve Connelly <eokuwwy@gmail.com>
2019-08-31 19:32:25 -0500

    Added release script

---
+++ build-release.sh
@@ -0,0 +1,15 @@
+OUTPUT_DIR=./output/specs
+mkdir -p $OUTPUT_DIR
+cp ./specs/json/*.json $OUTPUT_DIR
+cd ./output
+zip -r all-specs-json.zip specs
+mv all-specs-json.zip ../
+cd ../
+rm $OUTPUT_DIR/*.json
+cp ./specs/yml/*.yml $OUTPUT_DIR
+cd ./output
+zip -r all-specs-yml.zip specs
+mv all-specs-yml.zip ../
+cd ../
+rm -rf ./output
+
