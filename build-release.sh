f419cecdd8ae7b7cf2e742a6734e0a71f9800e14
Steve Connelly <eokuwwy@gmail.com>
2021-10-02 11:04:21 -0500

    Corrected max value for NRPN filter freq in OB-6 and Prophet-6, per confirmation from manufacturer support

--- build-release.sh
+++ build-release.sh
@@ -2,12 +2,14 @@ OUTPUT_DIR=./output/specs
 mkdir -p $OUTPUT_DIR
 cp ./specs/*/json/*.json $OUTPUT_DIR
 cd ./output
+echo "$1" > specs/release_notes.txt
 zip -r all-specs-json.zip specs
 mv all-specs-json.zip ../
 cd ../
 rm $OUTPUT_DIR/*.json
 cp ./specs/*/yml/*.yml $OUTPUT_DIR
 cd ./output
+echo "$1" > specs/release_notes.txt
 zip -r all-specs-yml.zip specs
 mv all-specs-yml.zip ../
 cd ../
